aa6a045c6476c9a5cf8c21ba720c31fe147dce0a
Lico-Tom <583566138@qq.com>
2022-10-20 16:59:55 +0800

    add pod ip.

--- docker-build/scripts/start-minio-cluster.sh
+++ docker-build/scripts/start-minio-cluster.sh
@@ -17,7 +17,11 @@ done
 
 mkdir -p $MINIO_HOME/logs
 if [ -z "$MINIO_DELVE_DEBUG" ] || [ $MINIO_DELVE_DEBUG == "false" ]; then
-  nohup minio server --address ${POD_NAME}.minio:9000  $DATA_CMD >>$MINIO_HOME/logs/minio.stdout.log 2>>$MINIO_HOME/logs/minio.stderr.log &
+  if [ -n "$POD_IP" ]; then
+       nohup minio server --address ${POD_IP}:9000  $DATA_CMD >>$MINIO_HOME/logs/minio.stdout.log 2>>$MINIO_HOME/logs/minio.stderr.log &
+  else
+       nohup minio server --address ${POD_NAME}:9000  $DATA_CMD >>$MINIO_HOME/logs/minio.stdout.log 2>>$MINIO_HOME/logs/minio.stderr.log &
+  fi
 else
   nohup dlv --listen=:2345 --headless=true --api-version=2 exec minio server $DATA_CMD >>$MINIO_HOME/logs/minio.stdout.log 2>>$MINIO_HOME/logs/minio.stderr.log &
 fi
