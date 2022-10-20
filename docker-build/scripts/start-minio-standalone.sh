e04cbb29ffd703075e83417e022050ddb78523c5
Lico-Tom <583566138@qq.com>
2022-10-20 15:27:46 +0800

    fix show log debug level.

--- docker-build/scripts/start-minio-standalone.sh
+++ docker-build/scripts/start-minio-standalone.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p $MINIO_HOME/logs
-if [ -z "$MINIO_DELVE_DEBUG" ] && [ $MINIO_DELVE_DEBUG == "false" ]; then
+if [ -z "$MINIO_DELVE_DEBUG" ] || [ $MINIO_DELVE_DEBUG == "false" ]; then
     nohup minio server $MINIO_HOME/data >>$MINIO_HOME/logs/minio.stdout.log 2>>$MINIO_HOME/logs/minio.stderr.log &
 else
     nohup dlv --listen=:2345 --headless=true --api-version=2 exec minio server $MINIO_HOME/data >>$MINIO_HOME/logs/minio.stdout.log 2>>$MINIO_HOME/logs/minio.stderr.log &
