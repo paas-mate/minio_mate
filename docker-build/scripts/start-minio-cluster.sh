e2d88c8542072cdcbd28b9d03bd6403c1aa49862
goflutterjava <goflutterjava@gmail.com>
2022-11-07 14:17:48 +0800

    config minio console address

--- docker-build/scripts/start-minio-cluster.sh
+++ docker-build/scripts/start-minio-cluster.sh
@@ -20,10 +20,10 @@ sed -i 's/localhost/'"${locationIp}"'/g' /root/.s3cfg
 mkdir -p $MINIO_HOME/logs
 if [ -z "$MINIO_DELVE_DEBUG" ] || [ $MINIO_DELVE_DEBUG == "false" ]; then
   if [ -n "$POD_IP" ]; then
-       nohup minio server --address ${POD_IP}:9000  $DATA_CMD >>$MINIO_HOME/logs/minio.stdout.log 2>>$MINIO_HOME/logs/minio.stderr.log &
+       nohup minio server --address ${POD_IP}:9000 --console-address 0.0.0.0:9001 $DATA_CMD >>$MINIO_HOME/logs/minio.stdout.log 2>>$MINIO_HOME/logs/minio.stderr.log &
   else
-       nohup minio server --address ${POD_NAME}:9000  $DATA_CMD >>$MINIO_HOME/logs/minio.stdout.log 2>>$MINIO_HOME/logs/minio.stderr.log &
+       nohup minio server --address ${POD_NAME}:9000 --console-address 0.0.0.0:9001 $DATA_CMD >>$MINIO_HOME/logs/minio.stdout.log 2>>$MINIO_HOME/logs/minio.stderr.log &
   fi
 else
-  nohup dlv --listen=:2345 --headless=true --api-version=2 exec minio server $DATA_CMD >>$MINIO_HOME/logs/minio.stdout.log 2>>$MINIO_HOME/logs/minio.stderr.log &
+  nohup dlv --listen=:2345 --headless=true --api-version=2 exec minio server --console-address 0.0.0.0:9001 $DATA_CMD >>$MINIO_HOME/logs/minio.stdout.log 2>>$MINIO_HOME/logs/minio.stderr.log &
 fi
--- docker-build/scripts/start-minio-standalone.sh
+++ docker-build/scripts/start-minio-standalone.sh
@@ -2,7 +2,7 @@
 
 mkdir -p $MINIO_HOME/logs
 if [ -z "$MINIO_DELVE_DEBUG" ] || [ $MINIO_DELVE_DEBUG == "false" ]; then
-    nohup minio server --address ${POD_NAME}.minio:9000 $MINIO_HOME/data >>$MINIO_HOME/logs/minio.stdout.log 2>>$MINIO_HOME/logs/minio.stderr.log &
+    nohup minio server --console-address 0.0.0.0:9001 $MINIO_HOME/data >>$MINIO_HOME/logs/minio.stdout.log 2>>$MINIO_HOME/logs/minio.stderr.log &
 else
-    nohup dlv --listen=:2345 --headless=true --api-version=2 exec minio server $MINIO_HOME/data >>$MINIO_HOME/logs/minio.stdout.log 2>>$MINIO_HOME/logs/minio.stderr.log &
+    nohup dlv --listen=:2345 --headless=true --api-version=2 exec minio server --console-address 0.0.0.0:9001 $MINIO_HOME/data >>$MINIO_HOME/logs/minio.stdout.log 2>>$MINIO_HOME/logs/minio.stderr.log &
 fi
